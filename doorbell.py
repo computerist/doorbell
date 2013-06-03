f7aeea8af76205c3fc6a3f672ba4c20277047efa
Mark Goodwin <mgoodwin@mozilla.com>
2013-06-03 07:45:29 +0100

    added some messages

--- doorbell.py
+++ doorbell.py
@@ -4,21 +4,26 @@ import time
 import urllib
 import twit
 
+messages = [line.strip() for line in open('messages','r').readlines()]
+
 # GPIO pin 9 takes the bell push switch
 PUSH = 9
 
 GPIO.setmode(GPIO.BCM) #numbering scheme that corresponds to breakout board and pin layout
-
 GPIO.setup(PUSH,GPIO.IN)
+
+rings = 0
+
 while True:
   start = time.time()
   while GPIO.input(PUSH):
     pass
   end = time.time()
   if end-start > 0.2:
+    rings = rings + 1
     try:
       urllib.urlopen('http://mini.local:8888/')
     except:
       pass
     # TODO: switch between messages
-    twit.tweet('@mr_goodwin Ding dong!')
+    twit.tweet('@mr_goodwin '+messages[rings%len(messages)])
